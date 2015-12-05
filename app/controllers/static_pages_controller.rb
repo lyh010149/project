4a19892146b6acb26c0653387dbe97428c3deb1d
Li Yihan <1160550968@qq.com>
2015-12-05 22:27:44 +0000

    addimage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   	if logged_in?
-      @micropost  = current_user.microposts.build
+      @micropost  = current_user.microposts.build if logged_in?
       @feed_items = current_user.feed
     end
   end
