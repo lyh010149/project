1968cd9df6c93d9377b001f174c08eb06f4e01cb
Li Yihan <1160550968@qq.com>
2015-12-08 17:56:25 +0000

    seed.rb change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -60,8 +60,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,:password_confirmation)
     end
     # Confirms a logged-in user.
     def logged_in_user
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,27 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Users table
-User.create(name:"liyihan00", email:"116055096800@qq.com", password:"000000", password_confirmation:"000000")
-User.create(name:"liyihan01", email:"116055096801@qq.com", password:"010101", password_confirmation:"010101")
-User.create(name:"liyihan02", email:"116055096802@qq.com", password:"020202", password_confirmation:"020202")
+User.create(name:'liyihan00', email:'116055096800@qq.com', password:"000000", password_confirmation:"000000")
+User.create(name:'liyihan01', email:'116055096801@qq.com', password:"010101", password_confirmation:"010101")
+User.create(name:'liyihan02', email:'116055096802@qq.com', password:"020202", password_confirmation:"020202")
 
 
 #Micropost table
 user = User.find(1)
-user.microposts.create(content:"This is Icecream!", picture:"IceCream.png")
+user.microposts.create(content:"This is Icecream!")
 user = User.find(2)
-user.microposts.create(content:"This is Apple Chip!", picture:"AppleChip.png")
+user.microposts.create(content:"This is Apple Chip!")
 user = User.find(3)
-user.microposts.create(content:"This is Mushroom Soup!", picture:"MushroomSoup.png")
+user.microposts.create(content:"This is Mushroom Soup!")
 
-# Following relationships
-Relationship.create(follower_id:"2", followed_id:"1")
-Relationship.create(follower_id:"3", followed_id:"1")
-Relationship.create(follower_id:"1", followed_id:"2")
-Relationship.create(follower_id:"1", followed_id:"3")
+#relationship table
+user1 = User.find(1)
+user2 = User.find(2)
+user3 = User.find(3)
+user2.follow(user1)
+user3.follow(user1)
+user1.follow(user2)
+user3.follow(user2)
+user2.follow(user3)
+
+
